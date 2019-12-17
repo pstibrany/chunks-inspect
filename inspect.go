956f11023f79277c815d633793574cac095bcd70
Peter Štibraný <peter.stibrany@grafana.com>
2019-12-17 12:58:29 +0100

    Chunks-inspect tool

---
+++ inspect.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+const format = "2006-01-02 15:04:05.000000 MST"
+
+var timezone = time.UTC
+
+func main() {
+	lines := flag.Bool("l", false, "print log lines")
+	flag.Parse()
+
+	for _, f := range flag.Args() {
+		printFile(f, *lines)
+	}
+}
+
+func printFile(filename string, printLines bool) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Printf("%s: %v", filename, err)
+		return
+	}
+	defer f.Close()
+
+	si, err := f.Stat()
+	if err != nil {
+		log.Println("failed to stat file", err)
+		return
+	}
+
+	h, err := DecodeHeader(f)
+	if err != nil {
+		log.Printf("%s: %v", filename, err)
+		return
+	}
+
+	fmt.Println("UserID:", h.UserID)
+	fmt.Println("From:", h.From.Time().In(timezone).Format(format))
+	fmt.Println("Through:", h.Through.Time().In(timezone).Format(format))
+	fmt.Println("Labels:")
+
+	for _, l := range h.Metric {
+		fmt.Println("\t", l.Name, "=", l.Value)
+	}
+
+	lokiChunk, err := parseLokiChunk(h, f)
+	if err != nil {
+		log.Printf("%s: %v", filename, err)
+	}
+
+	fmt.Println("Encoding:", lokiChunk.encoding)
+	fmt.Println("Found", len(lokiChunk.blocks), "block(s)")
+	fmt.Println()
+
+	totalSize := 0
+
+	for ix, b := range lokiChunk.blocks {
+		fmt.Printf("Block %4d: offset: %8x, original length: %6d (stored: %6d, ratio: %0.3g), minT: %v maxT: %v\n",
+			ix, b.dataOffset, b.uncompressedLength, b.dataLength, float64(b.uncompressedLength)/float64(b.dataLength),
+			time.Unix(0, b.minT).In(timezone).Format(format), time.Unix(0, b.maxT).In(timezone).Format(format))
+
+		totalSize += b.uncompressedLength
+
+		if printLines {
+			for _, l := range b.entries {
+				fmt.Printf("%v\t%s\n", time.Unix(0, l.timestamp).In(timezone).Format(format), strings.TrimSpace(l.line))
+			}
+		}
+	}
+
+	fmt.Println("Total size of uncompressed data:", totalSize, "file size:", si.Size(), "ratio:", fmt.Sprintf("%0.3g\n", float64(totalSize)/float64(si.Size())))
+}
---
+++ loki.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"github.com/golang/snappy"
+	"github.com/pierrec/lz4"
+)
+
+type Encoding struct {
+	code     int
+	name     string
+	readerFn func(io.Reader) (io.Reader, error)
+}
+
+func (e Encoding) String() string {
+	return e.name
+}
+
+var (
+	encNone   = Encoding{code: 0, name: "none", readerFn: func(reader io.Reader) (io.Reader, error) { return reader, nil }}
+	encGZIP   = Encoding{code: 1, name: "gzip", readerFn: func(reader io.Reader) (io.Reader, error) { return gzip.NewReader(reader) }}
+	encDumb   = Encoding{code: 2, name: "dumb", readerFn: func(reader io.Reader) (io.Reader, error) { return reader, nil }}
+	encLZ4    = Encoding{code: 3, name: "lz4", readerFn: func(reader io.Reader) (io.Reader, error) { return lz4.NewReader(reader), nil }}
+	encSnappy = Encoding{code: 4, name: "snappy", readerFn: func(reader io.Reader) (io.Reader, error) { return snappy.NewReader(reader), nil }}
+
+	Encodings = []Encoding{encNone, encGZIP, encDumb, encLZ4, encSnappy}
+)
+
+type LokiChunk struct {
+	encoding Encoding
+
+	blocks []LokiBlock
+}
+
+type LokiBlock struct {
+	numEntries uint64 // number of log lines in this block
+	minT       int64  // minimum timestamp, unix nanoseconds
+	maxT       int64  // max timestamp, unix nanoseconds
+
+	dataOffset uint64 // ofset in the data-part of chunks file
+	dataLength uint64 // length of raw data
+
+	rawData []byte // data as stored in chunk file, compressed
+
+	// parsed rawData
+	entries            []LokiEntry
+	uncompressedLength int
+}
+
+type LokiEntry struct {
+	timestamp int64
+	line      string
+}
+
+func parseLokiChunk(chunkHeader *ChunkHeader, r io.Reader) (*LokiChunk, error) {
+	// Loki chunks need to be loaded into memory, because some offsets are actually stored at the end.
+	data := make([]byte, chunkHeader.DataLength)
+	if _, err := io.ReadFull(r, data); err != nil {
+		return nil, fmt.Errorf("failed to read rawData for Loki chunk into memory: %w", err)
+	}
+
+	if num := binary.BigEndian.Uint32(data[0:4]); num != 0x012EE56A {
+		return nil, fmt.Errorf("invalid magic number: %0x", num)
+	}
+
+	format := data[4]
+	compression, err := getCompression(format, r)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read compression:", err)
+	}
+
+	// return &LokiChunk{encoding: compression}, nil
+
+	metasOffset := binary.BigEndian.Uint64(data[len(data)-8:])
+
+	metadata := data[metasOffset : len(data)-(8+4)]
+	blocks, n := binary.Uvarint(metadata)
+	if n <= 0 {
+		return nil, fmt.Errorf("failed to read number of blocks")
+	}
+	metadata = metadata[n:]
+
+	lokiChunk := &LokiChunk{
+		encoding: compression,
+	}
+
+	for ix := 0; ix < int(blocks); ix++ {
+		block := LokiBlock{}
+		block.numEntries, metadata, err = readUvarint(err, metadata)
+		block.minT, metadata, err = readVarint(err, metadata)
+		block.maxT, metadata, err = readVarint(err, metadata)
+		block.dataOffset, metadata, err = readUvarint(err, metadata)
+		block.dataLength, metadata, err = readUvarint(err, metadata)
+
+		if err != nil {
+			return nil, err
+		}
+
+		block.rawData = data[block.dataOffset : block.dataOffset+block.dataLength]
+		block.uncompressedLength, block.entries, err = parseLokiBlock(compression, block.rawData)
+		lokiChunk.blocks = append(lokiChunk.blocks, block)
+	}
+
+	return lokiChunk, nil
+}
+
+func parseLokiBlock(compression Encoding, data []byte) (int, []LokiEntry, error) {
+	r, err := compression.readerFn(bytes.NewReader(data))
+	if err != nil {
+		return 0, nil, err
+	}
+
+	decompressed, err := ioutil.ReadAll(r)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	entries := []LokiEntry(nil)
+	decompressedLen := len(decompressed)
+	for len(decompressed) > 0 {
+		var timestamp int64
+		var lineLength uint64
+
+		timestamp, decompressed, err = readVarint(err, decompressed)
+		lineLength, decompressed, err = readUvarint(err, decompressed)
+		if err != nil {
+			return 0, nil, err
+		}
+
+		if len(decompressed) < int(lineLength) {
+			return 0, nil, fmt.Errorf("not enough line data, need %d, got %d", lineLength, len(decompressed))
+		}
+
+		entries = append(entries, LokiEntry{
+			timestamp: timestamp,
+			line:      string(decompressed[0:lineLength]),
+		})
+
+		decompressed = decompressed[lineLength:]
+	}
+
+	return decompressedLen, entries, nil
+}
+
+func readVarint(prevErr error, buf []byte) (int64, []byte, error) {
+	if prevErr != nil {
+		return 0, buf, prevErr
+	}
+
+	val, n := binary.Varint(buf)
+	if n <= 0 {
+		return 0, nil, fmt.Errorf("varint: %d", n)
+	}
+	return val, buf[n:], nil
+}
+
+func readUvarint(prevErr error, buf []byte) (uint64, []byte, error) {
+	if prevErr != nil {
+		return 0, buf, prevErr
+	}
+
+	val, n := binary.Uvarint(buf)
+	if n <= 0 {
+		return 0, nil, fmt.Errorf("varint: %d", n)
+	}
+	return val, buf[n:], nil
+}
+
+func getCompression(format byte, r io.Reader) (Encoding, error) {
+	if format == 1 {
+		return encGZIP, nil
+	}
+
+	if format == 2 {
+		b := make([]byte, 1)
+		_, err := io.ReadFull(r, b)
+		if err != nil {
+			return encNone, err
+		}
+
+		for _, e := range Encodings {
+			if e.code == int(b[0]) {
+				return e, nil
+			}
+		}
+
+		return encNone, fmt.Errorf("unknown encoding: %d", b[0])
+	}
+
+	return encNone, fmt.Errorf("unknown format: %d", format)
+}
